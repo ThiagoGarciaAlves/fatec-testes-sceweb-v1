ae19d3594acfbfa13a962a11952b70ec5e85db17
Thiago G. Alves <thiagogarciaalves@mail.com>
2015-11-07 09:54:18 -0200

    Adicionados mais testes

--- Scewe_bv1/src/br/sceweb/model/Convenio.java
+++ Scewe_bv1/src/br/sceweb/model/Convenio.java
@@ -4,7 +4,30 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import org.joda.time.DateTime;
+
 public class Convenio {
+	
+	private String cnpj;
+	private DateTime dataInicio;
+	private DateTime dataTerminino;
+	
+	/**
+	* valida a data de inicio
+	* @param dataInicio - data do inicio da vigencia de um convenio
+	* @throws IllegalArgumentException
+	*/
+	public void setDataInicio(String dataInicio) throws IllegalArgumentException {
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>data inicio = " + dataInicio);
+		if (validaData(dataInicio)) {
+			this.dataInicio = new DateTime(Integer.parseInt(dataInicio.substring(6, 10)),
+			Integer.parseInt(dataInicio.substring(3, 5)),
+			Integer.parseInt(dataInicio.substring(0, 2)), 0, 0);
+		} else {
+			throw new IllegalArgumentException("data invalida");
+		}
+	}
+	
 	/**
 	* valida o formato da data
 	* @param data no formato dd/MM/yyyy
@@ -21,4 +44,29 @@ public class Convenio {
 			return false;
 		}
 	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public DateTime getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(DateTime dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public DateTime getDataTerminino() {
+		return dataTerminino;
+	}
+
+	public void setDataTerminino(DateTime dataTerminino) {
+		this.dataTerminino = dataTerminino;
+	}
+	
 }
--- Scewe_bv1/src/br/sceweb/teste/UC05CadastrarConvenio.java
+++ Scewe_bv1/src/br/sceweb/teste/UC05CadastrarConvenio.java
@@ -2,26 +2,59 @@ package br.sceweb.teste;
 
 import static org.junit.Assert.*;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import br.sceweb.model.Convenio;
+import br.sceweb.model.ConvenioDAO;
 
 public class UC05CadastrarConvenio {
 	
+	static ConvenioDAO convenioDAO;
 	static Convenio convenio;
 	static String dataInicio;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		convenioDAO = new ConvenioDAO();
 		convenio = new Convenio();
 		dataInicio = "29/10/2015";
 	}
-
+	
 	@Test
-	public void test() {
+	public void CT01UC05CadastrarConvenio_com_sucesso_data_valida() {
+		dataInicio = "29/10/2015";
 		assertTrue(convenio.validaData(dataInicio));
+	}
+	
+	@Test
+	public void CT02UC05CadastrarConvenio_com_data_invalida() {
+		dataInicio = "42/04/2015";
+		assertFalse(convenio.validaData(dataInicio));
+	}
+	
+	@Test
+	public void CT03UC05CadastrarConvenio_com_sucesso_data_valida() {
+		dataInicio = "29/10/2015";
+		convenio.setDataInicio(dataInicio);
+		assertTrue(dataInicio.equals(convenio.getDataInicio().toString("dd/MM/yyyy")));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void CT04UC05CadastrarConvenio_com_data_invalida() {
+		convenio.setDataInicio("242/10/2015");
+	}
+	
+	@Test
+	public void CT05UC05CadastrarConvenio_com_sucesso() {
+		convenioDAO.adiciona(convenio);
+		assertEquals(1, convenioDAO.adiciona(convenio));
+		convenioDAO.exclui(convenio.getCnpj());
 	}
 
 	@AfterClass
